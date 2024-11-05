80d4ac8c0c7fb5a456c998f0c4f79153a0539b0b
Sivasankar300 <155282396+Sivasankar300@users.noreply.github.com>
2024-11-05 19:39:02 +0530

    Add nameEntry and gameReset functionality

--- app.js
+++ app.js
@@ -1,130 +1,255 @@
-const gameBoard = (function () {
-    let gameBoardArray = [];
+let gameBoard = (function () {
+    let gameBoardArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
-    //Creating the Gameboard
-
-    let cells = 9;
-    for (cells; cells > 0; cells--) {
-        gameBoardArray.push("");
-    }
     return gameBoardArray;
 })();
 
-let player1 = newPlayer("Ajax", "X", 0);
-let player2 = newPlayer("NotAjax", "O", 0);
+const newGame = (function () {
+    const content = document.querySelector(".content");
+    const square = document.querySelectorAll(".square");
+    const info = document.querySelector(".info");
+    const startGameButton = document.querySelector(".startGame");
+    const player1Name = document.querySelector("#player1");
+    const player2Name = document.querySelector("#player2");
+    const submitForm = document.querySelector("#submitForm");
+    const form = document.querySelector("form");
+    const formParent = document.querySelector(".nameEntry")
 
-function newGame() {
-    
+
+    let player1;
+    let player2;
+
     let gameOver = 0;
     let currentPlayer;
     let playerInput;
 
-    function moveVaildity() {
-        //To prevent moves being made on already marked tiles
-        if ("X" === gameBoard[playerInput] || "O" === gameBoard[playerInput]) {
-            currentPlayer.turn--;
-            turnAssignment();            
-            return console.log("Invalid move");
-        }
-    }
-
-    function turnAssignment(){
-      currentPlayer = player1;
+    function turnAssignment() {
+        currentPlayer = player1;
         if (player1.turn > player2.turn) {
             currentPlayer = player2;
         }
     }
 
     function winCondition() {
-      //row1
-      if(gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X"){console.log(`${currentPlayer.playerName} has won`)
-        gameOver++}
-          else if(gameBoard[0] === "O" && gameBoard[1] === "O" && gameBoard[2] === "O"){console.log(`${currentPlayer.playerName} has won`)
-          gameOver++}
-      //row2
-      if(gameBoard[3] === "X" && gameBoard[4] === "X" && gameBoard[5] === "X"){console.log(`${currentPlayer.playerName} has won`)
-        gameOver++}
-          else if(gameBoard[3] === "O" && gameBoard[4] === "O" && gameBoard[5] === "O"){console.log(`${currentPlayer.playerName} has won`)
-          gameOver++}
-      //row3
-      if(gameBoard[6] === "X" && gameBoard[7] === "X" && gameBoard[8] === "X"){console.log(`${currentPlayer.playerName} has won`)
-        gameOver++}
-          else if(gameBoard[6] === "O" && gameBoard[7] === "O" && gameBoard[8] === "O"){console.log(`${currentPlayer.playerName} has won`)
-          gameOver++}
-
-      //Checking cols and dialognals, col 1
-      if(gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X"){console.log(`${currentPlayer.playerName} has won`)
-      gameOver++}
-        else if(gameBoard[0] === "O" && gameBoard[3] === "O" && gameBoard[6] === "O"){console.log(`${currentPlayer.playerName} has won`)
-        gameOver++}
-        //col 2
-      if(gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard[7] === "X"){console.log(`${currentPlayer.playerName} has won`)
-      gameOver++}
-        else if(gameBoard[1] === "O" && gameBoard[4] === "O" && gameBoard[7] === "O"){console.log(`${currentPlayer.playerName} has won`)
-        gameOver++}
-        //col 3
-      if(gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X"){console.log(`${currentPlayer.playerName} has won`)
-      gameOver++}
-        else if(gameBoard[2] === "O" && gameBoard[5] === "O" && gameBoard[8] === "O"){console.log(`${currentPlayer.playerName} has won`)
-        gameOver++}
-        //diag 1
-      if(gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X"){console.log(`${currentPlayer.playerName} has won`)
-      gameOver++}
-        else if(gameBoard[0] === "O" && gameBoard[3] === "O" && gameBoard[6] === "O"){console.log(`${currentPlayer.playerName} has won`)
-        gameOver++}
-        //diag 2
-      if(gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X"){console.log(`${currentPlayer.playerName} has won`)
-      gameOver++}
-        else if(gameBoard[0] === "O" && gameBoard[3] === "O" && gameBoard[6] === "O"){console.log(`${currentPlayer.playerName} has won`)
-        gameOver++}
-      
-        else if(!gameBoard.includes("E")){
-          console.log("It's a Tie!")
-          gameOver++
+        //row1
+        if (
+            gameBoard[0] === "X" &&
+            gameBoard[1] === "X" &&
+            gameBoard[2] === "X"
+        ) {
+            displayResult("won");
+            gameOver++;
+        } else if (
+            gameBoard[0] === "O" &&
+            gameBoard[1] === "O" &&
+            gameBoard[2] === "O"
+        ) {
+            displayResult("won");
+            gameOver++;
         }
-      }
-    
-    
-    while (gameOver === 0) {
-      /* Assign currentplayer
-    Ask user for input
-    Check if move is valid
-    Check if input is a number
-    Increment player's turn
-    Assign the input to the gameBoard
-    Check the win condition
-    console log the tabel */
-    turnAssignment();
-      playerInput = parseInt(window.prompt(`Which position would you like to mark ${currentPlayer.playerName}`));
+        //row2
+        else if (
+            gameBoard[3] === "X" &&
+            gameBoard[4] === "X" &&
+            gameBoard[5] === "X"
+        ) {
+            displayResult("won");
+            gameOver++;
+        } else if (
+            gameBoard[3] === "O" &&
+            gameBoard[4] === "O" &&
+            gameBoard[5] === "O"
+        ) {
+            displayResult("won");
+            gameOver++;
+        }
+        //row3
+        else if (
+            gameBoard[6] === "X" &&
+            gameBoard[7] === "X" &&
+            gameBoard[8] === "X"
+        ) {
+            displayResult("won");
+            gameOver++;
+        } else if (
+            gameBoard[6] === "O" &&
+            gameBoard[7] === "O" &&
+            gameBoard[8] === "O"
+        ) {
+            displayResult("won");
+            gameOver++;
+        }
 
-      moveVaildity();
-      //To break the loop with an Invalid input
-      if (isNaN(playerInput)) {
-        break;
+        //Checking cols and dialognals, col 1
+        else if (
+            gameBoard[0] === "X" &&
+            gameBoard[3] === "X" &&
+            gameBoard[6] === "X"
+        ) {
+            displayResult("won");
+            gameOver++;
+        } else if (
+            gameBoard[0] === "O" &&
+            gameBoard[3] === "O" &&
+            gameBoard[6] === "O"
+        ) {
+            displayResult("won");
+            gameOver++;
+        }
+        //col 2
+        else if (
+            gameBoard[1] === "X" &&
+            gameBoard[4] === "X" &&
+            gameBoard[7] === "X"
+        ) {
+            displayResult("won");
+            gameOver++;
+        } else if (
+            gameBoard[1] === "O" &&
+            gameBoard[4] === "O" &&
+            gameBoard[7] === "O"
+        ) {
+            displayResult("won");
+            gameOver++;
+        }
+        //col 3
+        else if (
+            gameBoard[2] === "X" &&
+            gameBoard[5] === "X" &&
+            gameBoard[8] === "X"
+        ) {
+            displayResult("won");
+            gameOver++;
+        } else if (
+            gameBoard[2] === "O" &&
+            gameBoard[5] === "O" &&
+            gameBoard[8] === "O"
+        ) {
+            displayResult("won");
+            gameOver++;
+        }
+        //diag 1
+        else if (
+            gameBoard[0] === "X" &&
+            gameBoard[4] === "X" &&
+            gameBoard[8] === "X"
+        ) {
+            displayResult("won");
+            gameOver++;
+        } else if (
+            gameBoard[0] === "O" &&
+            gameBoard[4] === "O" &&
+            gameBoard[8] === "O"
+        ) {
+            displayResult("won");
+            gameOver++;
+        }
+        //diag 2
+        else if (
+            gameBoard[2] === "X" &&
+            gameBoard[4] === "X" &&
+            gameBoard[6] === "X"
+        ) {
+            displayResult("won");
+            gameOver++;
+        } else if (
+            gameBoard[2] === "O" &&
+            gameBoard[4] === "O" &&
+            gameBoard[6] === "O"
+        ) {
+            displayResult("won");
+            gameOver++;
+        } else if (!gameBoard.includes(" ")) {
+            displayResult("tie");
+            gameOver++;
+        }
+
+        function displayResult(type) {
+            switch (type) {
+                case "won":
+                    info.textContent = `${currentPlayer.playerName} has 'won'! Press start game for a new round`;
+                    break;
+                case "tie":
+                    info.textContent = `It's a tie!
+                    Press start game for a new round`;
+                    break;
+            }
+        }
     }
 
-      currentPlayer.turn++;
-      gameBoard[playerInput] = currentPlayer.playerMarker;
-      console.table(gameBoard);
-      winCondition();
-      displayController();
+    startGameButton.addEventListener("click",() => {
+      //Also restarts the game
+      gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+
+      square.forEach(item => {
+        item.textContent = " ";
+      })
+      player1.turn = 0;
+      player2.turn = 0;
+      gameOver = 0;
+      
+      let fiveSecond = 5000;
+      setTimeout(info.textContent = " ", fiveSecond);
+    })
+
+    const nameEntry = (function(){
+      submitForm.addEventListener("click",(event) => {
+        event.preventDefault()
+        player1 = newPlayer(`${player1Name.value}`, "X",0);
+        player2 = newPlayer(`${player2Name.value}`, "O",0);
+        clearInput();
+    })
+        function clearInput(){
+        formParent.removeChild(form);
+        }
+    }())
+
+    const userInput = (function () {
+        content.addEventListener("click", (event) => {
+            if(gameOver>0){
+                return;
+            }
+            
+            turnAssignment();
+            //Gets position of the square
+            pointer = event.target.classList[1];
+
+            playerInput = pointer;
+
+            //TO-DO: Fix bug where if too many invalid moves are made, then the turn assignment stops working
+
+            //Move vaildity
+            if (
+                "X" === gameBoard[playerInput] ||
+                "O" === gameBoard[playerInput]
+            ) {
+                if (currentPlayer.turn) currentPlayer.turn--;
+                turnAssignment();
+                return console.log("Invalid move");
+            }
+
+            event.target.textContent = currentPlayer.playerMarker;
+            gameBoard[pointer] = currentPlayer.playerMarker;
+
+            currentPlayer.turn++;
+
+            winCondition();
+        });
+
+
+    })();
+
+
+
+    function newPlayer(playerName, playerMarker, turn) {
+        return { playerName, playerMarker, turn };
     }
-}
 
-function newPlayer(playerName, playerMarker, turn) {
-    return { playerName, playerMarker, turn };
-}
-
-
-const content = document.querySelector(".content");
-const boxes = document.querySelectorAll(".boxes");
-function displayController(){
-  count=0;
-  boxes.forEach(item => {
-    item.textContent = gameBoard[count];
-    count++
-  })
-
-}
-displayController();
-console.table(gameBoard);
+    function displayController() {
+        count = 0;
+        square.forEach((item) => {
+            item.textContent = gameBoard[count];
+            count++;
+        });
+    }
+})();
